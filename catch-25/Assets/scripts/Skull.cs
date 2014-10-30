b3651505b7ffbf34e8105142c17e5073e67c5d73
Lucas <lucas.cosson@metropolia.com>
2014-10-30 18:55:10 +0200

    Skull anim

---
+++ catch-25/Assets/scripts/Skull.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skull : MonoBehaviour {
+
+    [SerializeField]
+    private Animator anim;
+
+    public void SetSkullOpen(bool value)
+    {
+        anim.SetBool("opening", value);
+    }
+}
