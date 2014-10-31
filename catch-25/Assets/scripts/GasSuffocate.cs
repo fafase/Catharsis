d12998d21dfe35fe60cf2f5351fbf8fd75eabed1
Ville Pelkonen <lumisielu@gmail.com>
2014-10-31 11:24:48 +0200

    DOnt bother

---
+++ catch-25/Assets/scripts/GasSuffocate.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GasSuffocate : MonoBehaviour {
+
+	[SerializeField]
+	private bool gasOn = true;
+	private ParticleSystem gasParticles;
+	private Transform checkPosition;
+	private Transform player;
+	private float range;
+
+	void Start()
+	{
+		player = GameObject.Find("Cat").transform;
+		range = 1.5f;
+		gasParticles = transform.parent.Find ("GasParticles").particleSystem;
+	}
+
+	// Object enters suffocate zone
+	void OnTriggerEnter2D(Collider2D target) {
+		if (target.gameObject.transform == player || target.gameObject.transform == player) {
+			gasOn = false;
+			gasParticles.startLifetime = 1.0f;
+			gasParticles.startSpeed = 0.2f;
+		}	
+	}
+
+	// Object leaves suffocate zone
+		void OnTriggerExit2D(Collider2D target) {
+		if (target.gameObject.transform == player || target.gameObject.transform == player) {
+			gasOn = true;
+			gasParticles.startLifetime = 3.0f;
+			gasParticles.startSpeed = 1.0f;
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Audio distance volume
+		float distance = Vector2.Distance(transform.position, player.position);
+		transform.audio.volume = Mathf.Clamp(4f - distance, 0f, 0.4f);
+	}
+}
---
+++ catch-25/Assets/scripts/General/SetRespawn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetRespawn : MonoBehaviour
+{
+	[SerializeField]
+	private Transform medusa;
+	[SerializeField]
+	private GameObject player;
+	[SerializeField]
+	private bool active = true;
+
+	void Start()
+	{
+		medusa = GameObject.Find("Jelly").transform;
+		player = GameObject.Find("Cat");
+	}
+
+	void OnTriggerEnter2D(Collider2D target) {
+		if (target.gameObject == player && active) {
+			AudioManager.Instance.PlayAudio(Utility.SOUND_RESPAWN,0.7f,0.7f);
+			medusa.position = new Vector3 (transform.position.x, transform.position.y, 0);
+			active = false;
+		}	
+	}
+
+	void Update()
+	{
+
+	}
+
+}
