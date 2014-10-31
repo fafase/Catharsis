f439d45da76c7a0a6abce74ca3f510f4cdc1d8e6
Lucas Cosson <lucas.cosson@metropolia.fi>
2014-10-31 15:50:51 +0200

    Flower

---
+++ catch-25/Assets/Flower.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flower : DeathTrigger 
+{
+
+	protected abstract void CollisionCall (Collision2D col)
+	{
+		if (col.gameObject.CompareTag ("Player")) 
+		{
+			
+		}
+	}
+}
